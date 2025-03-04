8d0a46d789dc4cfdbae335f77e992ad0645ee68e
harsshgpt <harshgupta1752003@gmail.com>
2025-03-04 12:40:41 +0530

    descp

--- JS/hofs.js
+++ JS/hofs.js
@@ -1,3 +1,7 @@
+//clouser :- clouser are those who return a function and that returned function uses any variable of parent function
+//call back function:- a function which is passed as a parameter in any function
+//hofs :- higher order function are those who take any function as a parameneter or return any function 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
 function abcd(ac){
     ac();
 }
