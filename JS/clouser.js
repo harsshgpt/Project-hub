6bd7c4506f518f0810d01a79f69edc3165e3df24
harsshgpt <harshgupta1752003@gmail.com>
2025-03-04 23:55:21 +0530

    clouser new

--- JS/clouser.js
+++ JS/clouser.js
@@ -1,15 +1,51 @@
 //here i am creating counter using a clouser
 
-function count (){
-    let ct = 0 ;
-    return (function(){
-        ct++;
-        console.log(ct);
+// function count (){
+//     let ct = 0 ;
+//     return (function(){
+//         ct++;
+//         console.log(ct);
         
-    })
+//     })
+// }
+
+// let rct = count();
+
+// rct()
+// rct()
+
+//again using limiter
+
+function limiter (fn , limit ){
+    let lmt = 0
+    return function(){
+      if(lmt <limit){
+        lmt++;
+        fn();
+      }else{
+        console.log("Buy Pro to Watch More")
+      }
+    }
 }
+ 
+let fn = limiter(function(){
+    console.log("Hi i am Harsh ")
+},10)
 
-let rct = count();
-
-rct()
-rct()
+fn()
+fn()
+fn()
+fn()
+fn()
+fn()
+fn()
+fn()
+fn()
+fn()
+fn()
+fn()
+fn()
+fn()
+fn()
+fn()
+fn()
