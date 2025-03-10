b167d26575dd586d462529f0742d619407dc48e7
harsshgpt <harshgupta1752003@gmail.com>
2025-03-10 20:13:05 +0530

    nedw

--- JS/test.js
+++ JS/test.js
@@ -1,7 +1,16 @@
-let a = Number(prompt("enter a value "))
-let b = Number(prompt("enter b value "))
+let unit = Number(prompt("Enter Unit Here"));
+console.info("Printing Your Bill");
 
+let price = 0;
 
-console.log("the value of a is " +"= "+  a )
-console.log("the value of b is " +"= "+  b)
+if (unit <= 100) {
+    price = unit * 4.2;
+} else if (unit <= 200) {
+    price = (100 * 4.2) + ((unit - 100) * 6);
+} else if (unit <= 400) {
+    price = (100 * 4.2) + (100 * 6) + ((unit - 200) * 8);
+} else {
+    price = (100 * 4.2) + (100 * 6) + (200 * 8) + ((unit - 400) * 13);
+}
 
+console.log("Total Bill: ₹" + price.toFixed(2));
