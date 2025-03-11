9f41c110cd78e29aa5c8442761bd14e96178c2ad
harsshgpt <harshgupta1752003@gmail.com>
2025-03-11 15:15:00 +0530

    b

--- JS/test.js
+++ JS/test.js
@@ -2,7 +2,10 @@ let n = Number(prompt("enter a number"));
 
 for(let i = 1; i<=n;i++){
   if(n%i ==0 ){
-    console.log(i);
+    if(i == 1 || i == n){
+        continue;
+    }else{
+    console.log(i);}
     
   }
 }
