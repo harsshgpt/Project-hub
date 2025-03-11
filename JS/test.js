936c5c70ef0dbe47bbaeabbe072377eddda1b2f3
harsshgpt <harshgupta1752003@gmail.com>
2025-03-11 15:12:51 +0530

    factor finder

--- JS/test.js
+++ JS/test.js
@@ -1,11 +1,8 @@
 let n = Number(prompt("enter a number"));
-let evensum = 0 ;
-let oddsum = 0 ;
-for(let i = 1;i<=n;i++){
-    if(i%2 == 0){
-     evensum += i;
-    }else{
-       oddsum +=i;
-    }
+
+for(let i = 1; i<=n;i++){
+  if(n%i ==0 ){
+    console.log(i);
+    
+  }
 }
-console.log(`The even sum of ${n} is ${evensum} and the odd sum of ${n} id ${oddsum}`);
