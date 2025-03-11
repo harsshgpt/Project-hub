a3a4927cc55e744ed2b3fd189f9b6821a12b9c58
harsshgpt <harshgupta1752003@gmail.com>
2025-03-11 15:37:05 +0530

    changes

--- JS/test.js
+++ JS/test.js
@@ -1,11 +1,19 @@
 let n = Number(prompt("enter a number"));
-
-for(let i = 1; i<=n;i++){
+let sum = 0 ;
+for(let i = 1; i<=n/2;i++){
   if(n%i ==0 ){
     if(i == 1 || i == n){
         continue;
     }else{
+        sum++;
     console.log(i);}
     
   }
+}
+if(sum == 0 ){
+    console.log("prime");
+    
+}else{
+    console.log("Non prime");
+    
 }
