b982f03eaa9664f00512945bb263ffa90c5f1de0
harsshgpt <harshgupta1752003@gmail.com>
2025-03-15 02:01:46 +0530

    interview

--- JS/advloop.js
+++ JS/advloop.js
@@ -1,1 +1,53 @@
-let num = Math.floor((Math.random()*100))+1
+// const arr = ["one","two","three"];
+// const str = "Harsh";
+
+// const res = arr.includes("on")
+
+// const an = arr.includes("onetwo")
+// const newvar = str.includes("ar")
+
+// console.log(res);
+// console.log(an);
+// console.log(newvar);
+// console.log(+true);
+
+
+//++++++++++++++++++++++++++++++++++++++++ Merging two string
+// let str1 = "Hello";
+// let str2 = "World 1234";
+
+// function merge(str1,str2){
+//     let res = ""
+//     let m  ;
+//     for(let i = 0 ; i<str1.length && i<str2.length;i++){
+//       res += str1[i] + str2[i];
+//       m = i ;
+//     }
+//   m= m+1;
+//   while(m<str1.length || m<str2.length){
+//     if(m<str1.length ){
+//       res += str1[m];
+//       m++;
+//     }else{
+//       res += str2[m];
+//       m++;
+//     }
+//   }
+//   return res;
+// }
+// console.log(merge(str1,str2));
+
+
+
+
+// function sum(a,b){
+//   return a+b;
+// }
+// console.log(sum (2,4));
+
+// console.log(sum((2)(4)));
+
+
+const arr = [[1,2],[3,4],[5,6]]
+const res = arr.flat()
+console.log(res);
