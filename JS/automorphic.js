ac0407cf712b9a9cdd6520f467146cfe82974568
harsshgpt <harshgupta1752003@gmail.com>
2025-03-19 14:50:14 +0530

    h

--- JS/automorphic.js
+++ JS/automorphic.js
@@ -1,15 +1,20 @@
-let n = Number(prompt("Enter Here a number"))
-let copy = n;
-let count = 0;
+//For checking automorphic
+// let n = Number(prompt("Enter Here a number"))
+// let copy = n;
+// let count = 0;
 
-while(n>0){
-    count++;
-    n = Math.floor(n/10);
-}
+// while(n>0){
+//     count++;
+//     n = Math.floor(n/10);
+// }
 
-let sqr = Math.pow(copy,2);
-let lsts = sqr % (Math.pow(10,count))
+// let sqr = Math.pow(copy,2);
+// let lsts = sqr % (Math.pow(10,count))
 
-if(lsts == copy){
-    console.log("Automorphic")
-}else console.log("NOt Automorphic")
+// if(lsts == copy){
+//     console.log("Automorphic")
+// }else console.log("NOt Automorphic")
+
+const prompt = require('prompt-sync')();
+let n = prompt("Eneter a number")
+console.log(n);
