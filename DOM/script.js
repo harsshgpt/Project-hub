fb64f89cbd3087bf2a8f82212fff01d22cef1631
harsshgpt <harshgupta1752003@gmail.com>
2025-03-29 00:48:36 +0530

    sd

--- DOM/script.js
+++ DOM/script.js
@@ -1,8 +1,8 @@
 let sel = document.querySelector('h1')
-sel.innerHTML = "Html is changed❤️❤️❤️"
+sel.innerHTML = "Html is changed"
 sel.style.color = "purple"
 sel.style.backgroundColor = "white "
 let body = document.querySelector("body")
 sel.addEventListener("click",()=>{
-    body.style.backgroundColor = "black"
+    body.style.backgroundColor = "red"
 })
