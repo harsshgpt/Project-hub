eb0ef0ee36627ecd1cab80cb948337c0c6c08898
harsshgpt <harshgupta1752003@gmail.com>
2025-04-03 22:42:29 +0530

    sdjjns

--- DOM/script.js
+++ DOM/script.js
@@ -1,9 +1,1 @@
-let box = document.querySelector('#box');
-let button = document.querySelector('button');
-let c = 0;
 
-button.addEventListener("click", () => {
-    c++;
-    box.style.backgroundColor = c % 2 !== 0 ? "red" : "wheat";
-    console.log(c);
-});
