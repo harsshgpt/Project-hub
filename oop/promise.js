1a6c4a0e07df87b6c6601fd6d93855d855d57827
harsshgpt <harshgupta1752003@gmail.com>
2025-05-02 16:08:10 +0530

    slknd

---
+++ oop/promise.js
@@ -0,0 +1,20 @@
+function step1(){
+    return new Promise(function(res,rej){
+        console.log("Step 1 executed");
+        res();
+    })
+}
+function step2(){
+    return new Promise(function(res,rej){
+        console.log("Step 2 executed");
+        res();
+    })
+}
+function step3(){
+    return new Promise(function(res,rej){
+        console.log("Step 3 executed");
+        res();
+    })
+}
+
+step1().then(step2).then(step3)
