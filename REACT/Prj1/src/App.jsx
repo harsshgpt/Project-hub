b38a6eb62d1b903e724675660a3368bb8e627bb4
harsshgpt <harshgupta1752003@gmail.com>
2025-05-07 22:57:57 +0530

    lskds

--- REACT/Prj1/src/App.jsx
+++ REACT/Prj1/src/App.jsx
@@ -1,4 +1,8 @@
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // rafce react arrow function export component   :------ to import boiler plate code of app.jsx
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 import React from 'react'
 
 const App = () => {
