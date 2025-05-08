382973c10309661c85039a08d9349dcd4f649ad4
harsshgpt <harshgupta1752003@gmail.com>
2025-05-08 14:36:21 +0530

    ih

--- REACT/Prj1/src/App.jsx
+++ REACT/Prj1/src/App.jsx
@@ -1,12 +1,17 @@
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // rafce react arrow function export component   :------ to import boiler plate code of app.jsx
+//<Fragment> tags are previously used in the place of<></> which basically use is to not show in real dom of browser 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import { Fragment } from "react"
 
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    
+    </>
   )
 }
 
