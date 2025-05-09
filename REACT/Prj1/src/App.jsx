f240251ed436c4bc2d9e48be3a39b2f37b908c1b
harsshgpt <harshgupta1752003@gmail.com>
2025-05-09 20:59:44 +0530

    mn

--- REACT/Prj1/src/App.jsx
+++ REACT/Prj1/src/App.jsx
@@ -3,12 +3,22 @@
 //<Fragment> tags are previously used in the place of<></> which basically use is to not show in real dom of browser 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+import { useState } from "react"
 
-import React from 'react'
+
+
 
 const App = () => {
+  const[name1,setname]  = useState("HARSH")
+  
+  let change = ()=>{
+    setname("Gupta ji")
+  }
+  
   return (
     <>
+    <h1>{name1}</h1>
+    <button onClick = {change}>change name1</button>
     </>
   )
 }
