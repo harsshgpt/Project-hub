5d5559c5292d065bfe3cf9f6724d92d9e3fc3309
harsshgpt <harshgupta1752003@gmail.com>
2025-05-14 18:49:56 +0530

    snasl

--- REACT/DAY1/src/App.jsx
+++ REACT/DAY1/src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react"
 
 
 function App() {
+  const [user,setuser] = useState([
+    {name:"HARSH", AGE:25},
+    {name:"JOHN", AGE:20},
+    {name:"Apex", AGE:28}
+
+  ]);
+  const render = user.map((user,index)=>{
+    return <li key={index}> {index +1} => {user.name}</li>;
+  })
+
   return (
    <>
-   
+     <h1>USERS NAME</h1>
+     <ol>{render}</ol>
    </>
   )
 }
