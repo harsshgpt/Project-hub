ac1501aaccbca8f487518bd09bb49e3638b3f038
harsshgpt <harshgupta1752003@gmail.com>
2025-05-14 20:38:28 +0530

    sds

--- REACT/DAY1/src/App.jsx
+++ REACT/DAY1/src/App.jsx
@@ -13,23 +13,35 @@ function App() {
   })
    
   const [fullname,setfullname] = useState("");
-  const changehandler = (e) =>{
-      setfullname ( e.target.value);
-  }
-  console.log(fullname)
+    const [age,setage] = useState(18);
+  
+    
+
+   const subch = (e)=>{
+      e.preventDefault();
+    const newuser={fullname,age} ;
+    console.log(newuser)
+    
+   }
+
+
 
   return (
    <>
-   
-   
      <h1>USERS NAME</h1>
-      <form >
-    <input
-       onChange={changehandler}
+      <form  onSubmit={subch}>
+    <input 
+       onChange={(e) =>setfullname ( e.target.value)}
        value={fullname}
        type="text" 
        placeholder="Username"
- 
+    />
+    <input
+   
+       onChange={(e) =>setage( e.target.value)}
+       value={age}
+       type="number" 
+       placeholder="AGE"
     />
     <button>Submit</button>
    </form>
