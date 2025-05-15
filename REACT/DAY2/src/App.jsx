2eb032ca999e1403b6c28f93013387b6ec07be2a
harsshgpt <harshgupta1752003@gmail.com>
2025-05-15 22:35:44 +0530

    two way binding

--- REACT/DAY2/src/App.jsx
+++ REACT/DAY2/src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+const App = () => {
+  const [value, setvalue]  = useState("");
+const[completed,setcompleted] = useState("")
+const[gender,setgender] = useState("");
+console.log(gender)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <form >
+      <input
+      value = {value}
+      onChange={(e)=>setvalue(e.target.value)}
+      type="text" />
+    </form>
+
+    <br />
+    <br />
+    <input
+    checked = {completed}
+    onChange={(e)=>setcompleted(e.target.checked)}
+    type="checkbox" />Checkbox
+
+    <br />
+    <br />
+
+    <input 
+    value="male"
+    onChange={(e)=>setgender(e.target.value)}
+    checked = {gender == "male" && true}
+    type="radio"
+     />Male
+    <input 
+    value="female"
+    onChange={(e)=>setgender(e.target.value)}
+    checked = {gender == "female" && true}
+    type="radio" />Female
+
     </>
+    
   )
 }
 
