406125401cfff28074b395892e4860b1fa84b7fb
harsshgpt <harshgupta1752003@gmail.com>
2025-05-15 22:58:25 +0530

    nanoid

--- REACT/DAY2/src/App.jsx
+++ REACT/DAY2/src/App.jsx
@@ -1,20 +1,34 @@
 import { useState } from "react"
-
+import { nanoid } from 'nanoid';
 
 
 const App = () => {
   const [value, setvalue]  = useState("");
+  
 
+  const submithandler = (e)=>{
+   e.preventDefault();
+   const newtodo ={
+    id :nanoid(),
+    value,
+    iscompleted :false
+   }
+   console.log(newtodo)
+   console.log("runned")
+  }
   return (
     <>
     <hr />
-    <form >
+    <form onSubmit={submithandler} >
       <input
       value = {value}
       onChange={(e)=>setvalue(e.target.value)}
       type="text" />
+      <br />
+       <button >Submit</button>
+       <hr />
     </form>
-    <button >Submit</button>
+   
 
     
 
