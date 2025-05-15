6cb2dc04a52cf998aa98fac9b94129ec32aa0987
harsshgpt <harshgupta1752003@gmail.com>
2025-05-15 22:44:18 +0530

    todos

--- REACT/DAY2/src/App.jsx
+++ REACT/DAY2/src/App.jsx
@@ -4,39 +4,19 @@ import { useState } from "react"
 
 const App = () => {
   const [value, setvalue]  = useState("");
-const[completed,setcompleted] = useState("")
-const[gender,setgender] = useState("");
-console.log(gender)
+
   return (
     <>
+    <hr />
     <form >
       <input
       value = {value}
       onChange={(e)=>setvalue(e.target.value)}
       type="text" />
     </form>
+    <button >Submit</button>
 
-    <br />
-    <br />
-    <input
-    checked = {completed}
-    onChange={(e)=>setcompleted(e.target.checked)}
-    type="checkbox" />Checkbox
-
-    <br />
-    <br />
-
-    <input 
-    value="male"
-    onChange={(e)=>setgender(e.target.value)}
-    checked = {gender == "male" && true}
-    type="radio"
-     />Male
-    <input 
-    value="female"
-    onChange={(e)=>setgender(e.target.value)}
-    checked = {gender == "female" && true}
-    type="radio" />Female
+    
 
     </>
     
