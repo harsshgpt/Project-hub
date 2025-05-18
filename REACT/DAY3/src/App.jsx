36316b872303091fee65f06b1c8d3a2ac90b640f
harsshgpt <harshgupta1752003@gmail.com>
2025-05-18 17:01:16 +0530

    ksj;ds

--- REACT/DAY3/src/App.jsx
+++ REACT/DAY3/src/App.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react"
+import { useForm } from "react-hook-form"
 function App() {
+  
+const {register,handleSubmit,watch,formState: { errors },} = useForm()
+const{todo,settodo} = useState();
+const SubmitHandler = (data)=>{
+  console.log(data)
+}
   return (
-    <div>App</div>
+   <>
+   <form onSubmit={handleSubmit(SubmitHandler)} >
+    <input type="text" {...register("title"),{required:"title cant be empety"}} placeholder="title" />
+    <small>{errors}</small>
+    <input type ="submit" />
+   </form>
+   </>
   )
 }
 
