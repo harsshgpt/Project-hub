e145328f9cfded6ab02071bfd06e78cd8eb6808f
harsshgpt <harshgupta1752003@gmail.com>
2025-05-18 17:49:50 +0530

    ../,

--- REACT/DAY2/src/main.jsx
+++ REACT/DAY2/src/main.jsx
@@ -1,11 +1,13 @@
 
 import { createRoot } from 'react-dom/client'
 import "./index.css"
+import {ToastConatainer} from "react-toastify";
 
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
- 
+ <>
     <App />
-  
+    <ToastConatainer position = "top-center"/>
+  </>
 )
