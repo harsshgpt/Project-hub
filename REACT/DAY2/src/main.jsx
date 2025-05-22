95cbab46f4615e6dbdabf364af40b726ae2aafe4
harsshgpt <harshgupta1752003@gmail.com>
2025-05-22 10:17:41 +0530

    used wrapper

--- REACT/DAY2/src/main.jsx
+++ REACT/DAY2/src/main.jsx
@@ -4,10 +4,8 @@ import "./index.css"
 import {ToastContainer} from "react-toastify";
 
 import App from './App.jsx'
+import wrapper from './wrapper.jsx';
 
-createRoot(document.getElementById('root')).render(
- <>
-    <App />
-    <ToastContainer position = "top-center"/>
-  </>
-)
+createRoot(document.getElementById('root')).render(<wrapper>
+  <App></App>
+</wrapper>)
---
+++ REACT/DAY2/src/wrapper.jsx
@@ -0,0 +1,6 @@
+
+function wrapper(props) {
+  return props.children
+}
+
+export default wrapper
